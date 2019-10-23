db647e12280739b58083ff4cd11d14582d6be26d
Jamie Wallingford <jamie.wallingford@capstonec.com>
2019-10-23 14:34:31 -0500

    temporary update to expose headers from gateway

--- Rosetta/ActionFilters/IpAddressCaptureActionFilter.cs
+++ Rosetta/ActionFilters/IpAddressCaptureActionFilter.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Rosetta.Services;
 
@@ -16,6 +18,16 @@ namespace Rosetta.ActionFilters
 
         public override void OnActionExecuted(ActionExecutedContext context)
         {
+            var keys = context.HttpContext.Request.Headers.Keys.ToArray();
+            var allHeaders = new List<string>();
+            foreach (var key in keys)
+            {
+                var value = context.HttpContext.Request.Headers[key];
+                allHeaders.Add($"{key}:{value}");
+            }
+
+            var headers = string.Join(',', allHeaders);
+            _service.Add(headers);
             _service.Add(context.HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString());
         }
     }
