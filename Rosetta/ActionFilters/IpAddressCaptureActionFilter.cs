43e5a4d194eafc268cbfbc99e26e6454555fe13e
Jamie Wallingford <jamie.wallingford@capstonec.com>
2019-10-23 15:14:44 -0500

    Removing unused usings

--- Rosetta/ActionFilters/IpAddressCaptureActionFilter.cs
+++ Rosetta/ActionFilters/IpAddressCaptureActionFilter.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Rosetta.Services;
