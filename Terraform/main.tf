fdb94a80b8cb2d1f1f64dde4982e2e9f53690ecc
Jamie Wallingford <jamie.wallingford@capstonec.com>
2019-11-01 08:46:08 -0500

    commenting out app gateway terraform and leaving a lengthly comment on why

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -247,91 +247,101 @@ resource "azurerm_application_insights" "rosettastone-ai" {
   application_type    = "web"
 }
 
-resource "azurerm_virtual_network" "rosettastonev2-vnet" {
-  name                = "hisc-${var.app_environment}-${var.app_name}-vnet"
-  resource_group_name = var.default_resource_group
-  location            = var.default_location
-  address_space       = ["10.6.0.0/16"]
-}
-
-resource "azurerm_subnet" "frontend-subnet" {
-  name                = "hisc-${var.app_environment}-${var.app_name}-subnet-fe"
-  resource_group_name = var.default_resource_group
-  virtual_network_name = "${azurerm_virtual_network.rosettastonev2-vnet.name}"
-  address_prefix      = "10.6.0.0/24"
-}
-
-resource "azurerm_public_ip" "public-ip" {
-  name                = "hisc-${var.app_environment}-${var.app_name}-pip"
-  resource_group_name = var.default_resource_group
-  location            = var.default_location
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_application_gateway" "rosettastonev2-ag" {
-  name                = "hisc-${var.app_environment}-${var.app_name}-ag"
-  resource_group_name = var.default_resource_group
-  location            = var.default_location
-
-  sku {
-    name     = "Standard_v2"
-    tier     = "Standard_v2"
-    capacity = 1
-  }
-
-  gateway_ip_configuration {
-    name      = "ag-ip-configuration" 
-    subnet_id = "${azurerm_subnet.frontend-subnet.id}"
-  }
-
-  frontend_port {
-    name = "${azurerm_virtual_network.rosettastonev2-vnet.name}-fe-port"
-    port = 4567
-  }
-
-  frontend_ip_configuration {
-    name                 = "${azurerm_virtual_network.rosettastonev2-vnet.name}-fe-ip"
-    public_ip_address_id = "${azurerm_public_ip.public-ip.id}"
-  }
-
-  backend_address_pool {
-    name  = "${azurerm_virtual_network.rosettastonev2-vnet.name}-be-ap"
-    fqdns = ["${azurerm_app_service.rosettastone-as.name}.azurewebsites.net"]
-  }
-
-  backend_http_settings {
-    name                                = "${azurerm_virtual_network.rosettastonev2-vnet.name}-be-hs"
-    cookie_based_affinity               = "Disabled"
-    protocol                            = "Https"
-    port                                = 443
-    request_timeout                     = 90
-    pick_host_name_from_backend_address = true
-    probe_name                          = "${azurerm_virtual_network.rosettastonev2-vnet.name}-probe"
-  }
-
-  http_listener {
-    name                           = "${azurerm_virtual_network.rosettastonev2-vnet.name}-http-lstn"
-    frontend_ip_configuration_name = "${azurerm_virtual_network.rosettastonev2-vnet.name}-fe-ip"
-    frontend_port_name             = "${azurerm_virtual_network.rosettastonev2-vnet.name}-fe-port"
-    protocol                       = "Http" # this should be https but we need a cert first
-  }
-
-  request_routing_rule {
-    name                       = "${azurerm_virtual_network.rosettastonev2-vnet.name}-rq-rt"
-    rule_type                  = "Basic"
-    http_listener_name         = "${azurerm_virtual_network.rosettastonev2-vnet.name}-http-lstn"
-    backend_address_pool_name  = "${azurerm_virtual_network.rosettastonev2-vnet.name}-be-ap"
-    backend_http_settings_name = "${azurerm_virtual_network.rosettastonev2-vnet.name}-be-hs"
-  }
-
-  probe {
-    name                                      = "${azurerm_virtual_network.rosettastonev2-vnet.name}-probe"
-    protocol                                  = "Https"
-    pick_host_name_from_backend_http_settings = true
-    path                                      = "/health"
-    interval                                  = 30
-    timeout                                   = 30
-    unhealthy_threshold                       = 3
-  }
-}
+# the following has been commented out since we do not need to have a gateway to port forward requests
+# to the app service. the original rosetta stone used sinatra and was deployed using port 4567
+# this project originally expected to be a "drop in" replacement, and was expected to use port 4567.
+# azure app services as of 2019-11-01 only allow for port 80 and 443 to be exposed, therefore we
+# added an app gateway to listen on 4567 and forward to the app service on 443. this worked and
+# was functional. however when moving to prod, it was determined that the calling services (app stack and webinquiry)
+# would be able to call the new app service directly, therefore the requirment for the app gateway,
+# custom ssl certs and dns entries was no longer a requirment. i am leaving this terraform to create the
+# app gateway here and commented so that if it would be determined later that a unknown calling system requires
+# us to function in a "drop in" fashion, the resources to do so are readily and easily created.
+#resource "azurerm_virtual_network" "rosettastonev2-vnet" {
+#  name                = "hisc-${var.app_environment}-${var.app_name}-vnet"
+#  resource_group_name = var.default_resource_group
+#  location            = var.default_location
+#  address_space       = ["10.6.0.0/16"]
+#}
+#
+#resource "azurerm_subnet" "frontend-subnet" {
+#  name                = "hisc-${var.app_environment}-${var.app_name}-subnet-fe"
+#  resource_group_name = var.default_resource_group
+#  virtual_network_name = "${azurerm_virtual_network.rosettastonev2-vnet.name}"
+#  address_prefix      = "10.6.0.0/24"
+#}
+#
+#resource "azurerm_public_ip" "public-ip" {
+#  name                = "hisc-${var.app_environment}-${var.app_name}-pip"
+#  resource_group_name = var.default_resource_group
+#  location            = var.default_location
+#  allocation_method   = "Static"
+#  sku                 = "Standard"
+#}
+#
+#resource "azurerm_application_gateway" "rosettastonev2-ag" {
+#  name                = "hisc-${var.app_environment}-${var.app_name}-ag"
+#  resource_group_name = var.default_resource_group
+#  location            = var.default_location
+#
+#  sku {
+#    name     = "Standard_v2"
+#    tier     = "Standard_v2"
+#    capacity = 1
+#  }
+#
+#  gateway_ip_configuration {
+#    name      = "ag-ip-configuration" 
+#    subnet_id = "${azurerm_subnet.frontend-subnet.id}"
+#  }
+#
+#  frontend_port {
+#    name = "${azurerm_virtual_network.rosettastonev2-vnet.name}-fe-port"
+#    port = 4567
+#  }
+#
+#  frontend_ip_configuration {
+#    name                 = "${azurerm_virtual_network.rosettastonev2-vnet.name}-fe-ip"
+#    public_ip_address_id = "${azurerm_public_ip.public-ip.id}"
+#  }
+#
+#  backend_address_pool {
+#    name  = "${azurerm_virtual_network.rosettastonev2-vnet.name}-be-ap"
+#    fqdns = ["${azurerm_app_service.rosettastone-as.name}.azurewebsites.net"]
+#  }
+#
+#  backend_http_settings {
+#    name                                = "${azurerm_virtual_network.rosettastonev2-vnet.name}-be-hs"
+#    cookie_based_affinity               = "Disabled"
+#    protocol                            = "Https"
+#    port                                = 443
+#    request_timeout                     = 90
+#    pick_host_name_from_backend_address = true
+#    probe_name                          = "${azurerm_virtual_network.rosettastonev2-vnet.name}-probe"
+#  }
+#
+#  http_listener {
+#    name                           = "${azurerm_virtual_network.rosettastonev2-vnet.name}-http-lstn"
+#    frontend_ip_configuration_name = "${azurerm_virtual_network.rosettastonev2-vnet.name}-fe-ip"
+#    frontend_port_name             = "${azurerm_virtual_network.rosettastonev2-vnet.name}-fe-port"
+#    protocol                       = "Http" # this should be https but we need a cert first
+#  }
+#
+#  request_routing_rule {
+#    name                       = "${azurerm_virtual_network.rosettastonev2-vnet.name}-rq-rt"
+#    rule_type                  = "Basic"
+#    http_listener_name         = "${azurerm_virtual_network.rosettastonev2-vnet.name}-http-lstn"
+#    backend_address_pool_name  = "${azurerm_virtual_network.rosettastonev2-vnet.name}-be-ap"
+#    backend_http_settings_name = "${azurerm_virtual_network.rosettastonev2-vnet.name}-be-hs"
+#  }
+#
+#  probe {
+#    name                                      = "${azurerm_virtual_network.rosettastonev2-vnet.name}-probe"
+#    protocol                                  = "Https"
+#    pick_host_name_from_backend_http_settings = true
+#    path                                      = "/health"
+#    interval                                  = 30
+#    timeout                                   = 30
+#    unhealthy_threshold                       = 3
+#  }
+#}
