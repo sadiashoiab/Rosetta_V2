9a331eb53ec795d8b824ae7117d8586810e3418e
Jamie Wallingford <jamie.wallingford@capstonec.com>
2019-11-20 09:07:13 -0600

    cleaning up usings

--- ClearCareOnline.Api/AgencyMapper.cs
+++ ClearCareOnline.Api/AgencyMapper.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using ClearCareOnline.Api.Models;
 using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
 
 namespace ClearCareOnline.Api
 {
